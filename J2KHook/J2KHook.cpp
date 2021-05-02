002beb6445282af748d3a732593e38bfd9b17914
sokcuri <sokcuri@gmail.com>
2021-05-03 05:38:53 +0900

    impl: trie_record_jis::ctor - update gitignore

--- J2KHook/J2KHook.cpp
+++ J2KHook/J2KHook.cpp
@@ -2,18 +2,13 @@
 #include <windows.h>
 #include "subhook.h"
 
-using cstring_t = void*;
-subhook_t foo_hook;
-
-HMODULE module_mfc;
-
-LPVOID __0CString__QAE_XZ				= (LPVOID)0x101F2A4A; // CString::CString(void *this)
-LPVOID __0CString__QAE_PBD_Z			= (LPVOID)0x101F2A68; // CString::CString(void *this, const char *)
-LPVOID __1CString__QAE_XZ				= (LPVOID)0x101F2A50; // CString::~CString(void *this)
-LPVOID _Left_CString__QBE_AV1_H_Z		= (LPVOID)0x101F2B04; // CString::Left(void *this, void **, int)
-LPVOID __H_YG_AVCString__ABV0_PBD_Z		= (LPVOID)0x101F2B5E; // operator+(CString const &,char const *)
-
-cstring_t t = nullptr;
+LPVOID __0CString__QAE_XZ					= (LPVOID)0x101F2A4A; // CString::CString(void *this)
+LPVOID __0CString__QAE_PBD_Z				= (LPVOID)0x101F2A68; // CString::CString(void *this, const char *)
+LPVOID __1CString__QAE_XZ					= (LPVOID)0x101F2A50; // CString::~CString(void *this)
+LPVOID _Left_CString__QBE_AV1_H_Z			= (LPVOID)0x101F2B04; // CString::Left(void *this, void **, int)
+LPVOID __H_YG_AVCString__ABV0_PBD_Z			= (LPVOID)0x101F2B5E; // operator+(CString const &,char const *)
+LPVOID __2_YAPAXI_Z							= (LPVOID)0x101F2A74; // void *__cdecl operator new(unsigned int a1)
+LPVOID _GetJ2KMainDir__YA_AVCString__XZ_0	= (LPVOID)0x101C5EA0; // CString *__cdecl GetJ2KMainDir(CString *a1)
 
 bool foo(int x) {
 	return true;
@@ -64,6 +59,32 @@ int CString_Left(void* _this, void* _new, int _count) {
 	return result;
 }
 
+void* OperatorNew(unsigned int _len) {
+	void *addr = 0;
+	
+	__asm {
+		push _len
+		call __2_YAPAXI_Z
+		add esp, 0x04
+		mov addr, eax
+	}
+
+	return addr;
+}
+
+void* __cdecl GetJ2KMainDir(void* a1) {
+	void* result = nullptr;
+
+	__asm {
+		push a1
+		call _GetJ2KMainDir__YA_AVCString__XZ_0
+		add esp, 0x04
+		mov result, eax
+	}
+
+	return result;
+}
+
 int CString_GetLength(void* _this) {
 	return *(int*)((char*)_this - 8);
 }
@@ -82,16 +103,34 @@ int Operator_CStringConstRef_CharConstPtr(void* _new, void* _left, const char *_
 	return result;
 }
 
-int fc2(void* _this, void *a2) {
+// _this = trie_record_jis
+// userdict_path = ***/UserDict.jk
+int sub_10002FFE(void* _this, void* userdict_path) {
+	int result = 0;
+	
+	__asm {
+		mov ecx, _this
+		push userdict_path
+		mov eax, 0x10002FFE
+		call eax
+		mov result, eax
+	}
 
+	return result;
+}
+
+int trie_record_jis_Constructor(void* _this, void *a2) {
+
+	void* v15 = nullptr;
 	void* v17 = nullptr;
 	void* v171 = nullptr;
 	void* v18 = nullptr;
+	void* v19 = nullptr;
 	void* lpFileName = nullptr;
 
-	CString_Constructor((LPVOID*)((DWORD *)_this + 8));
-	CString_Constructor((LPVOID*)((DWORD *)_this + 12));
-	CString_Constructor((LPVOID*)((DWORD *)_this + 60));
+	CString_Constructor((LPVOID*)((DWORD *)&_this + 8));
+	CString_Constructor((LPVOID*)((DWORD *)&_this + 12));
+	CString_Constructor((LPVOID*)((DWORD *)&_this + 60));
 
 	CString_Constructor(&v17);
 	CString_Destructor(&v17);
@@ -112,45 +151,24 @@ int fc2(void* _this, void *a2) {
 		if (hMap) {
 			LPVOID lpMapView = MapViewOfFile(hMap, 4u, 0, 0, 0);
 			*(DWORD*)((DWORD*)_this + 84) = (DWORD)lpMapView;
+			void* test = OperatorNew(0x18);
+
+			if (test) {
+
+				GetJ2KMainDir(&v19);
+				Operator_CStringConstRef_CharConstPtr(&v15, &v19, "UserDict.jk");
+				
+				int ret = sub_10002FFE(_this, v15);
+			}
 		}
 
 	}
-	
-	CString_Destructor((LPVOID*)((DWORD*)_this + 8));
-	CString_Destructor((LPVOID*)((DWORD*)_this + 12));
-	CString_Destructor((LPVOID*)((DWORD*)_this + 60));
+
+	CString_Destructor((LPVOID*)((DWORD*)&_this + 8));
+	CString_Destructor((LPVOID*)((DWORD*)&_this + 12));
+	CString_Destructor((LPVOID*)((DWORD*)&_this + 60));
+
 	return 0;
-}
-
-__declspec(naked) int __fastcall fc() {
-
-	__asm {
-		push ecx
-
-		push [esp + 0x04 + 0x04]
-		push ecx
-		call fc2
-		add esp, 0x08
-
-		mov ecx, [esp]
-		push [esp + 0x04 + 0x04]
-		mov eax, 0x101bba70
-		call eax
-
-		pop eax
-		retn 4
-	}
-}
-
-void my_foo(int x) {
-	/* Remove the hook so that you can call the original function. */
-	subhook_remove(foo_hook);
-
-	printf("foo(%d) called\n", x);
-	foo(x);
-
-	/* Install the hook back to intercept further calls. */
-	subhook_install(foo_hook);
 }
 
 int main() {
@@ -160,7 +178,7 @@ int main() {
 	const auto& trj_ctor = GetProcAddress(module_j2k, "??0trie_record_jis@@QAE@VCString@@@Z");
 	if (trj_ctor == 0) return 2;
 
-	auto trj_ctor_hook = subhook_new((void*)trj_ctor, (void*)fc, (subhook_flags_t)0);
+	auto trj_ctor_hook = subhook_new((void*)trj_ctor, (void*)trie_record_jis_Constructor, (subhook_flags_t)0);
 	subhook_install(trj_ctor_hook);
 
 	const auto& fnInit = (bool(__stdcall*)(const char *, const char *))GetProcAddress(module_j2k, "J2K_InitializeEx");
@@ -171,18 +189,7 @@ int main() {
 	const auto& fnTrans = (const char*(__stdcall*)(int, const char*))GetProcAddress(module_j2k, "J2K_TranslateMMNT");
 	printf("%s\n", fnTrans(0, "?맯궢궫귞긚깋귽?궬궯궫뙊"));
 
-
-	/* Create a hook that will redirect all foo() calls to to my_foo(). */
-	foo_hook = subhook_new((void*)foo, (void*)my_foo, (subhook_flags_t)0);
-
-	/* Install it. */
-		subhook_install(foo_hook);
-
-	foo(123);
-
-	/* Remove the hook and free memory when you're done. */
-	subhook_remove(foo_hook);
-	subhook_free(foo_hook);
-
+	subhook_remove(trj_ctor_hook);
+	subhook_free(trj_ctor_hook);
 	return 0;
 }
