ccf1f1ce2a3110c2a4f36925ceca157b739a73e3
sokcuri <sokcuri@gmail.com>
2021-05-03 22:05:17 +0900

    impl: trie_record_jis::ctor

--- J2KHook/J2KHook.cpp
+++ J2KHook/J2KHook.cpp
@@ -7,12 +7,13 @@ LPVOID __0CString__QAE_PBD_Z				= (LPVOID)0x101F2A68; // CString::CString(void *
 LPVOID __1CString__QAE_XZ					= (LPVOID)0x101F2A50; // CString::~CString(void *this)
 LPVOID _Left_CString__QBE_AV1_H_Z			= (LPVOID)0x101F2B04; // CString::Left(void *this, void **, int)
 LPVOID __H_YG_AVCString__ABV0_PBD_Z			= (LPVOID)0x101F2B5E; // operator+(CString const &,char const *)
+LPVOID __4CString__QAEABV0_ABV0__Z			= (LPVOID)0x101F2A3E; // int __thiscall CString::operator=(void *this, int a2)
 LPVOID __2_YAPAXI_Z							= (LPVOID)0x101F2A74; // void *__cdecl operator new(unsigned int a1)
 LPVOID _GetJ2KMainDir__YA_AVCString__XZ_0	= (LPVOID)0x101C5EA0; // CString *__cdecl GetJ2KMainDir(CString *a1)
-
-bool foo(int x) {
-	return true;
-}
+LPVOID _init_P_DA_trie_record_jis__AAEXXZ_0 = (LPVOID)0x101BD860; // void __thiscall trie_record_jis::init_P_DA(trie_record_jis *this)
+LPVOID _init_S_DA_trie_record_jis__AAEXXZ_0 = (LPVOID)0x101BCA60; // void __thiscall trie_record_jis::init_S_DA(trie_record_jis *this)
+LPVOID _init_LA_trie_record_jis__AAEXXZ_0	= (LPVOID)0x101BE600; // void __thiscall trie_record_jis::init_LA(trie_record_jis *this)
+LPVOID _init_rec_trie_record_jis__AAEXXZ_0	= (LPVOID)0x101BC710; // void __thiscall trie_record_jis::init_rec(trie_record_jis *this)
 
 int CString_Constructor(void* _this) {
 	int result = 0;
@@ -89,7 +90,9 @@ int CString_GetLength(void* _this) {
 	return *(int*)((char*)_this - 8);
 }
 
-int Operator_CStringConstRef_CharConstPtr(void* _new, void* _left, const char *_right) {
+// __4CString__QAEABV0_ABV0__Z
+
+int OperatorPlus_CStringConstRef_CharConstPtr(void* _new, void* _left, const char* _right) {
 	int result = 0;
 
 	__asm {
@@ -101,6 +104,47 @@ int Operator_CStringConstRef_CharConstPtr(void* _new, void* _left, const char *_
 		mov result, eax
 	}
 	return result;
+}
+
+int OperatorEqual_CStringConstRef_CStringConstRef(void* _this, void* _value) {
+	int result = 0;
+
+	__asm {
+		mov ecx, _this
+		push _value
+		call __4CString__QAEABV0_ABV0__Z
+
+		mov result, eax
+	}
+	return result;
+}
+
+void init_P_DA(void* _this) {
+	__asm {
+		mov ecx, _this
+		call _init_P_DA_trie_record_jis__AAEXXZ_0
+	}
+}
+
+void init_S_DA(void* _this) {
+	__asm {
+		mov ecx, _this
+		call _init_S_DA_trie_record_jis__AAEXXZ_0
+	}
+}
+
+void init_LA(void* _this) {
+	__asm {
+		mov ecx, _this
+		call _init_LA_trie_record_jis__AAEXXZ_0
+	}
+}
+
+void init_rec(void* _this) {
+	__asm {
+		mov ecx, _this
+		call _init_rec_trie_record_jis__AAEXXZ_0
+	}
 }
 
 // _this = trie_record_jis
@@ -135,11 +179,11 @@ int trie_record_jis_Constructor2(void* _this, void *a2) {
 	CString_Constructor(&v17);
 	CString_Destructor(&v17);
 	CString_Constructor(&v17, "testasdf");
-	Operator_CStringConstRef_CharConstPtr(&v171, &v17, "asdf.inf");
+	OperatorPlus_CStringConstRef_CharConstPtr(&v171, &v17, "asdf.inf");
 	CString_Destructor(&v17);
 
 	CString_Left(&a2, &v18, CString_GetLength(a2) - 3);
-	Operator_CStringConstRef_CharConstPtr(&lpFileName, &v18, ".inf");
+	OperatorPlus_CStringConstRef_CharConstPtr(&lpFileName, &v18, ".inf");
 	CString_Destructor(&v18);
 
 	HANDLE hFile = CreateFileA((char*)lpFileName, 0x80000000, 1u, 0, 3u, 0x80u, 0);
@@ -156,10 +200,18 @@ int trie_record_jis_Constructor2(void* _this, void *a2) {
 			if (test) {
 
 				GetJ2KMainDir(&v19);
-				Operator_CStringConstRef_CharConstPtr(&v15, &v19, "UserDict.jk");
+				OperatorPlus_CStringConstRef_CharConstPtr(&v15, &v19, "UserDict.jk");
 				
 				int ret = sub_10002FFE(_this, v15);
+				CString_Destructor(&v19);
 			}
+
+			*(DWORD*)((DWORD*)_this + 4) = (DWORD)test;
+			OperatorEqual_CStringConstRef_CStringConstRef((DWORD*)&_this + 12, &a2);
+			init_P_DA(_this);
+			init_S_DA(_this);
+			init_LA(_this);
+			init_rec(_this);
 		}
 
 	}
